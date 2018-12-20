1d3aa111a62f47737dc0cda76e5c99c47e032f87
Nathan <Nathan@Nathans-MacBook-Pro-2.local>
2018-12-19 17:21:15 -0800

    added git.ignore and git.attributes

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billValue: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func OnTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func billValueChanged(_ sender: Any) {
+        let billAmount = Double(billValue.text!) ?? 0
+        let tipP = [0.18,0.20,0.25]
+        
+        let tip = billAmount*tipP[tipPercent.selectedSegmentIndex]
+        let total = billAmount + tip
+        
+        
+        tipLabel.text = String(format: "S%0.2f",tip)
+        totalLabel.text = String(format:"$%0.2f",total)
+        
+        
+        
+    }
+    
+    
 }
 
